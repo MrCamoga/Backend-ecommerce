d9aeaa64c68134873a017dfb2a8b8770c2304d20
MrCamoga <mrcamoga@gmail.com>
2025-06-06 09:55:17 +0200

    Added authentication to product creation and deletion

--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const { getAll, getById, create, updateById, deleteById } = require("../controllers/ProductController");
+const { authentication } = require("../middleware/authentication");
+
 
 router.get("/", getAll); // Query params: price, minPrice, maxPrice, name, sort{ASC|DESC}
 router.get("/:id", getById);
-router.post("/", create);
-router.put("/:id", updateById);
-router.delete("/:id", deleteById);
+router.post("/", authentication, create);
+router.put("/:id", authentication, updateById);
+router.delete("/:id", authentication, deleteById);
 
 module.exports = router;
