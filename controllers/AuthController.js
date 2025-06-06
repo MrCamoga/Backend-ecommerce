b0ed4fa3f990b212a575672b8d87aed5702413b8
MrCamoga <mrcamoga@gmail.com>
2025-06-06 09:31:01 +0200

    Fixed await in not async method AuthController

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,8 +5,12 @@ const bcrypt = require('bcryptjs');
 const { jwt_secret } = require('../config/config.js')['development'];
 
 module.exports = {
-	login: (req,res,next) => {
-		User.findOne({ where: { email: req.body.email }, attributes: {include: ['first_name','last_name','email','createdAt']}}).then(user => {
+	login: async (req,res,next) => {
+		try {
+			const user = await User.findOne({
+				where: { email: req.body.email },
+				attributes: {include: ['first_name','last_name','email','createdAt']}
+			});
 			// dummy password if user is not found to prevent a timing attack
 			const passwordsEqual = bcrypt.compareSync(req.body.password, user.password ?? '$2a$10$j.NJYLehXvr/ehpgoTvQ0OO2N8as45Iv0JgZbSPf6lrpUmUAbFhfS');
 
@@ -15,13 +19,13 @@ module.exports = {
 			const tokenRecord = await Token.create({ UserId: user.id });
 			const token = jwt.sign({ id: user.id, token_id: tokenRecord.id }, jwt_secret);
 			res.send({message: `Welcome ${user.first_name}`, user, token});
-		}).catch(error => {
+		} catch(error) {
 			console.log(error)
 			res.status(500).send({message: 'Internal Server Error', error});
-		})
+		}
 	},
 
 	logout: (req,res,next) => {
-		
+
 	}
 };
